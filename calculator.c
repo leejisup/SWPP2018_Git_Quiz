68b47560f907021f79000410156ec5236720a657
leejisup <dlwltjq10@hanmail.net>
2018-03-15 17:19:32 +0900

    implement add, sub, and mul function

--- calculator.c
+++ calculator.c
@@ -23,13 +23,13 @@ int main(){
 }
 
 int add(int a, int b) {
-  return 0; // TODO : FIX IT
+  return a+b; // TODO : FIX IT
 }
 
 int sub(int a, int b) {
-  return 0; // TODO : FIX IT
+  return a-b; // TODO : FIX IT
 }
 
 int mul(int a, int b) {
-  return 0; // TODO : FIX IT
+  return a*b; // TODO : FIX IT
 }
